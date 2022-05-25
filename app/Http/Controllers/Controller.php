348a2a2f4a324c0d5c9b12aa7e1647880ab429c3
Esse Jacques Dasnomon <essedansomon@gmail.com>
2022-05-25 08:51:08 +0000

    Initialisation of the course

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\View\View;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index() : View
+    {
+        return view('welcome');
+    }
+
+    public function home()
+    {
+        return 'hello jacques';
+    }
+}
